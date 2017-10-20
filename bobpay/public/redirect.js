774e0380bd51c588f6dcd508d3a116139ce7136a
Adam Powers <apowers@ato.ms>
2017-10-20 09:00:03 -0400

    removing port number

--- bobpay/public/redirect.js
+++ bobpay/public/redirect.js
@@ -1,4 +1,4 @@
-if ((window.location.host === 'webauthn.org:8443') &&
+if ((window.location.host === 'webauthn.org') &&
     (window.location.protocol !== 'https:')) {
   window.location.protocol = 'https:';
 }
